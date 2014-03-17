b9d46f2fbb4a0ce07cecce44c755b4fae22fefd0
Techzune <techzune@operontech.com>
2014-03-17 00:09:17 +0000

    Added ID version of isSpecialBlock

--- src/main/java/com/operontech/redblocks/Util.java
+++ src/main/java/com/operontech/redblocks/Util.java
@@ -10,6 +10,11 @@ import org.bukkit.Material;
 public class Util {
 	private static final List<Material> specialBList = Arrays.asList(Material.ACTIVATOR_RAIL, Material.ANVIL, Material.BED_BLOCK, Material.BROWN_MUSHROOM, Material.CACTUS, Material.CAKE_BLOCK, Material.CROPS, Material.DETECTOR_RAIL, Material.DIODE_BLOCK_OFF, Material.DIODE_BLOCK_ON, Material.FLOWER_POT, Material.ITEM_FRAME, Material.SUGAR_CANE_BLOCK, Material.REDSTONE_WIRE, Material.REDSTONE_COMPARATOR_OFF, Material.REDSTONE_COMPARATOR_ON, Material.REDSTONE_TORCH_OFF, Material.REDSTONE_TORCH_ON, Material.PAINTING, Material.POWERED_RAIL, Material.WOOD_DOOR, Material.IRON_DOOR_BLOCK);
 
+	@SuppressWarnings("deprecation")
+	public static boolean isSpecialBlock(final int typeId) {
+		return isSpecialBlock(Material.getMaterial(typeId));
+	}
+
 	public static boolean isSpecialBlock(final Material m) {
 		return specialBList.contains(m);
 	}
