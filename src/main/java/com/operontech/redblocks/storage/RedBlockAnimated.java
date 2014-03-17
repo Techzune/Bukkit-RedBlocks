4e3ea71cc9673aaf1929dd2cabb39c8e30568a9f
Techzune <techzune@operontech.com>
2014-03-17 00:11:46 +0000

    Illustration Work

--- src/main/java/com/operontech/redblocks/storage/RedBlockAnimated.java
+++ src/main/java/com/operontech/redblocks/storage/RedBlockAnimated.java
@@ -1,8 +1,8 @@
 package com.operontech.redblocks.storage;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -28,8 +28,18 @@ public class RedBlockAnimated implements Serializable {
 	private boolean blocksActive;
 	private boolean inTimeout = false;
 	private boolean changed = false;
-	private final Map<RedBlockChild, Boolean> listOfBlocks = new TreeMap<RedBlockChild, Boolean>();
+	// RedBlockChild & Time to Pause
+	private final Map<RedBlockChild, Integer> listOfBlocks = new TreeMap<RedBlockChild, Integer>();
 
+	/**
+	 * Creates a RedBlock.
+	 * @param location the location of the physical block
+	 * @param type the material of the physical block
+	 * @param data the byte data of the physical block
+	 * @param owner the owner of the RedBlock
+	 * @param protect the "protect" option
+	 * @param inverted the "inverted" option
+	 */
 	public RedBlockAnimated(final Location location, final Material type, final byte data, final String owner, final boolean protect, final boolean inverted) {
 		this.location = Util.convertLocationToString(location);
 		this.owner = owner;
@@ -40,18 +50,72 @@ public class RedBlockAnimated implements Serializable {
 	}
 
 	/**
+	 * Creates a RedBlock.
+	 * @param b the physical block for the RedBlock
+	 * @param owner the owner of the RedBlock
+	 * @param protect the "protect" option
+	 * @param inverted the "inverted" option
+	 */
+	@SuppressWarnings("deprecation")
+	public RedBlockAnimated(final Block b, final String owner, final boolean protect, final boolean inverted) {
+		location = Util.convertLocationToString(b.getLocation());
+		type = b.getType();
+		data = b.getData();
+		this.owner = owner;
+		this.protect = protect;
+		this.inverted = inverted;
+	}
+
+	/**
+	 * Enables all of the RedBlockChilds in the RedBlock's database of blocks.
+	 * @param force if true, the blocks will cause block updates when enabled forcibly
+	 */
+	public void enable(final boolean force) {
+		final Set<Chunk> chunks = new HashSet<Chunk>();
+		if (!blocksActive || force) {
+			for (final Entry<RedBlockChild, Integer> entry : listOfBlocks.entrySet()) {
+				entry.getKey().enableBlock(Util.isSpecialBlock(entry.getKey().getTypeId()));
+				chunks.add(entry.getKey().getLocation().getChunk());
+			}
+			for (final Chunk chunk : chunks) {
+				chunk.load();
+				chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
+			}
+			blocksActive = true;
+		}
+	}
+
+	/**
+	 * Disables all of the RedBlockChilds in the RedBlock's database of blocks.
+	 * @param force if true, the blocks will cause block updates when disabled forcibly
+	 */
+	public void disable(final boolean force) {
+		final Set<Chunk> chunks = new HashSet<Chunk>();
+		if (blocksActive || force) {
+			for (final Entry<RedBlockChild, Integer> entry : listOfBlocks.entrySet()) {
+				entry.getKey().disableBlock(Util.isSpecialBlock(entry.getKey().getTypeId()));
+				chunks.add(entry.getKey().getLocation().getChunk());
+			}
+			for (final Chunk chunk : chunks) {
+				chunk.load();
+				chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
+			}
+			blocksActive = false;
+		}
+	}
+
+	/**
 	 * Adds a RedBlockChild to the RedBlock's database of blocks.
 	 * @param child the child to be added
 	 * @return the 
 	 */
 	@SuppressWarnings("deprecation")
-	public RedBlockChild add(final RedBlockChild child) {
+	public boolean add(final RedBlockChild child) {
 		if (Util.isSpecialBlock(Material.getMaterial(child.getTypeId()))) {
-			listOfBlocks.put(child, true);
+			return !listOfBlocks.put(child, 0).equals(child);
 		} else {
-			listOfBlocks.put(child, false);
+			return !listOfBlocks.put(child, 0).equals(child);
 		}
-		return child;
 	}
 
 	/**
@@ -62,8 +126,38 @@ public class RedBlockAnimated implements Serializable {
 	 * @return the converted RedBlockChild
 	 */
 	@SuppressWarnings("deprecation")
-	public RedBlockChild add(final Block b) {
+	public boolean add(final Block b) {
 		return add(new RedBlockChild(b.getTypeId(), b.getData(), b.getLocation()));
+	}
+
+	/**
+	 * Adds multiple RedBlockChilds to the RedBlock's database of blocks.
+	 * @param list the list of RedBlockChilds
+	 * @return the number of RedBlockChilds added
+	 */
+	public int addChildList(final List<RedBlockChild> list) {
+		int i = 0;
+		for (final RedBlockChild child : list) {
+			if (add(child)) {
+				i++;
+			}
+		}
+		return i;
+	}
+
+	/**
+	 * Adds multiple Blocks to the RedBlock's database of blocks.
+	 * @param list the list of Blocks
+	 * @return the number of Blocks added
+	 */
+	public int addBlockList(final List<Block> list) {
+		int i = 0;
+		for (final Block child : list) {
+			if (add(child)) {
+				i++;
+			}
+		}
+		return i;
 	}
 
 	/**
@@ -86,7 +180,8 @@ public class RedBlockAnimated implements Serializable {
 		final Set<RedBlockChild> rbcKeys = listOfBlocks.keySet();
 		for (final RedBlockChild rbc : rbcKeys) {
 			if (rbc.getLocation().toString().equals(b.getLocation().toString())) {
-				return listOfBlocks.remove(rbc);
+				listOfBlocks.remove(rbc);
+				return true;
 			}
 		}
 		return false;
@@ -97,7 +192,7 @@ public class RedBlockAnimated implements Serializable {
 	 * @param list the list of RedBlockChilds
 	 * @return the number of RedBlockChilds removed
 	 */
-	public int removeList(final ArrayList<RedBlockChild> list) {
+	public int removeChildList(final List<RedBlockChild> list) {
 		int i = 0;
 		for (final RedBlockChild rbc : list) {
 			if (remove(rbc)) {
@@ -108,41 +203,18 @@ public class RedBlockAnimated implements Serializable {
 	}
 
 	/**
-	 * Enables all of the RedBlockChilds in the RedBlock's database of blocks.
-	 * @param force if true, the blocks will cause block updates when enabled forcibly
+	 * Removes multiple Blocks from the RedBlock's database of blocks.
+	 * @param list the list of Blocks
+	 * @return the number of RedBlockChilds removed
 	 */
-	public void enable(final boolean force) {
-		final Set<Chunk> chunks = new HashSet<Chunk>();
-		if (!blocksActive || force) {
-			for (final Entry<RedBlockChild, Boolean> entry : listOfBlocks.entrySet()) {
-				entry.getKey().enableBlock(entry.getValue());
-				chunks.add(entry.getKey().getLocation().getChunk());
+	public int removeBlockList(final List<Block> list) {
+		int i = 0;
+		for (final Block rbc : list) {
+			if (remove(rbc)) {
+				i++;
 			}
-			for (final Chunk chunk : chunks) {
-				chunk.load();
-				chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
-			}
-			blocksActive = true;
 		}
-	}
-
-	/**
-	 * Disables all of the RedBlockChilds in the RedBlock's database of blocks.
-	 * @param force if true, the blocks will cause block updates when disabled forcibly
-	 */
-	public void disable(final boolean force) {
-		final Set<Chunk> chunks = new HashSet<Chunk>();
-		if (blocksActive || force) {
-			for (final Entry<RedBlockChild, Boolean> entry : listOfBlocks.entrySet()) {
-				entry.getKey().disableBlock(entry.getValue());
-				chunks.add(entry.getKey().getLocation().getChunk());
-			}
-			for (final Chunk chunk : chunks) {
-				chunk.load();
-				chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
-			}
-			blocksActive = false;
-		}
+		return i;
 	}
 
 	/**
@@ -350,7 +422,7 @@ public class RedBlockAnimated implements Serializable {
 	 * Gets the Map of the RedBlock's database of blocks.
 	 * @return the Map
 	 */
-	public Map<RedBlockChild, Boolean> getDatabase() {
+	public Map<RedBlockChild, Integer> getDatabase() {
 		return listOfBlocks;
 	}
 
@@ -361,446 +433,4 @@ public class RedBlockAnimated implements Serializable {
 	public Set<RedBlockChild> getBlocks() {
 		return listOfBlocks.keySet();
 	}
-
-	//
-	//	/**
-	//	 * Adds a block to the RedBlock.
-	//	 *
-	//	 * @param typeId the typeID of the block
-	//	 * @param data the data of the block
-	//	 * @param loc the location of the block
-	//	 * @return if the block was added
-	//	 */
-	//	public boolean add(final int typeId, final byte data, final Location loc) {
-	//		fixNulls();
-	//		if (contains(loc.getBlock())) {
-	//			return false;
-	//		}
-	//		if (specialBList.contains(loc.getBlock().getType())) {
-	//			return specialBlocks.add(new RedBlockChild(typeId, data, loc));
-	//		}
-	//		return blocks.add(new RedBlockChild(typeId, data, loc));
-	//	}
-	//
-	//	/**
-	//	 * Adds a block to the RedBlock.
-	//	 *
-	//	 * @param b the block to be added
-	//	 * @return if the block was added
-	//	 */
-	//	@SuppressWarnings("deprecation")
-	//	public boolean add(final Block b) {
-	//		fixNulls();
-	//		return add(b.getTypeId(), b.getData(), b.getLocation());
-	//	}
-	//
-	//	/**
-	//	 * Removes a block from the RedBlock.
-	//	 *
-	//	 * @param b the block to be removed
-	//	 * @return if the block was removed
-	//	 */
-	//	public boolean remove(final Block b) {
-	//		fixNulls();
-	//		final Set<RedBlockChild> rbc = new HashSet<RedBlockChild>();
-	//		final Set<RedBlockChild> rsbc = new HashSet<RedBlockChild>();
-	//		rbc.addAll(blocks);
-	//		rsbc.addAll(specialBlocks);
-	//		for (final RedBlockChild c : rbc) {
-	//			if (c.getLocation().toString().equals(b.getLocation().toString())) {
-	//				return blocks.remove(c);
-	//			}
-	//		}
-	//		for (final RedBlockChild c : rsbc) {
-	//			if (c.getLocation().toString().equals(b.getLocation().toString())) {
-	//				return specialBlocks.remove(c);
-	//			}
-	//		}
-	//		return false;
-	//	}
-	//
-	//	/**
-	//	 * Removes a RedBlockChild from the RedBlock.
-	//	 *
-	//	 * @param b the block to be removed
-	//	 * @return if the block was removed
-	//	 */
-	//	public boolean remove(final RedBlockChild b) {
-	//		fixNulls();
-	//		return blocks.remove(b) || specialBlocks.remove(b);
-	//	}
-	//
-	//	/**
-	//	 * Removes a RedBlockChild in a list from the RedBlock.
-	//	 *
-	//	 * @param children the children
-	//	 * @return the number of blocks removed
-	//	 */
-	//	public int removeList(final List<RedBlockChild> children) {
-	//		fixNulls();
-	//		final Set<RedBlockChild> rbc = new HashSet<RedBlockChild>();
-	//		final Set<RedBlockChild> rsbc = new HashSet<RedBlockChild>();
-	//		rbc.addAll(blocks);
-	//		rsbc.addAll(specialBlocks);
-	//		int removed = 0;
-	//		for (final RedBlockChild c : rbc) {
-	//			if (children.contains(c)) {
-	//				blocks.remove(c);
-	//				removed++;
-	//			}
-	//		}
-	//		for (final RedBlockChild c : rsbc) {
-	//			if (children.contains(c)) {
-	//				specialBlocks.remove(c);
-	//				removed++;
-	//			}
-	//		}
-	//		return removed;
-	//	}
-	//
-	//	/**
-	//	 * Enables the children of the RedBlock.
-	//	 *
-	//	 * @param force if true, the RedBlock will ignore if the RedBlock is already enabled and enable it again
-	//	 */
-	//	public void enable(final boolean force) {
-	//		final Set<Chunk> chunks = new HashSet<Chunk>();
-	//		if (!isEnabled() || force) {
-	//			fixNulls();
-	//			if ((blocks != null) && (blocks.size() > 0)) {
-	//				for (final RedBlockChild c : blocks) {
-	//					c.enableBlock(false);
-	//				}
-	//			}
-	//			if ((specialBlocks != null) && (specialBlocks.size() > 0)) {
-	//				for (final RedBlockChild c : specialBlocks) {
-	//					c.enableBlock(true);
-	//					chunks.add(c.getLocation().getChunk());
-	//				}
-	//			}
-	//			for (final Chunk chunk : chunks) {
-	//				chunk.load();
-	//				chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
-	//			}
-	//			blocksActive = true;
-	//		}
-	//	}
-	//
-	//	/**
-	//	 * Disables the children of the RedBlock.
-	//	 *
-	//	 * @param force if true, the RedBlock will ignore if the RedBlock is already disabled and disable it again
-	//	 */
-	//	public void disable(final boolean force) {
-	//		if (isEnabled() || force) {
-	//			final Set<Chunk> chunks = new HashSet<Chunk>();
-	//			fixNulls();
-	//			if ((specialBlocks != null) && (specialBlocks.size() > 0)) {
-	//				for (final RedBlockChild c : specialBlocks) {
-	//					c.disableBlock(true);
-	//					chunks.add(c.getLocation().getChunk());
-	//				}
-	//			}
-	//			if ((blocks != null) && (blocks.size() > 0)) {
-	//				for (final RedBlockChild c : blocks) {
-	//					c.disableBlock(false);
-	//					chunks.add(c.getLocation().getChunk());
-	//				}
-	//			}
-	//			for (final Chunk chunk : chunks) {
-	//				chunk.load();
-	//				chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
-	//			}
-	//			blocksActive = false;
-	//		}
-	//	}
-	//
-	//	/**
-	//	 * Checks if the RedBlock controls a block.
-	//	 *
-	//	 * @param b the block to check for
-	//	 * @return if the RedBlock contains that block
-	//	 */
-	//	public boolean contains(final Block b) {
-	//		fixNulls();
-	//		final Set<RedBlockChild> rbc = new HashSet<RedBlockChild>();
-	//		final Set<RedBlockChild> rsbc = new HashSet<RedBlockChild>();
-	//		rbc.addAll(blocks);
-	//		rsbc.addAll(specialBlocks);
-	//		for (final RedBlockChild c : rbc) {
-	//			if (c.getLocation().toString().equals(b.getLocation().toString())) {
-	//				return true;
-	//			}
-	//		}
-	//		for (final RedBlockChild c : rsbc) {
-	//			if (c.getLocation().toString().equals(b.getLocation().toString())) {
-	//				return true;
-	//			}
-	//		}
-	//		return false;
-	//	}
-	//
-	//	/**
-	//	 * Adds or Removes multiple blocks by using List<Block>.
-	//	 *
-	//	 * @param block the list of blocks
-	//	 * @param remove if true, the blocks in the list will be removed from the RedBlock, but if false, they will be added
-	//	 * @return the amount of blocks added/removed
-	//	 */
-	//	@SuppressWarnings("deprecation")
-	//	public int blockListAction(final List<Block> block, final boolean remove) {
-	//		fixNulls();
-	//		final Set<RedBlockChild> rbc = new HashSet<RedBlockChild>();
-	//		final Set<RedBlockChild> rsbc = new HashSet<RedBlockChild>();
-	//		rbc.addAll(blocks);
-	//		rsbc.addAll(specialBlocks);
-	//		int affected = 0;
-	//		for (final RedBlockChild rc : rbc) {
-	//			if (block.contains(rc.getBlock())) {
-	//				if (remove) {
-	//					blocks.remove(rc);
-	//					affected++;
-	//				} else {
-	//					block.remove(rc.getBlock());
-	//				}
-	//			}
-	//		}
-	//		for (final RedBlockChild rc : rsbc) {
-	//			if (block.contains(rc.getBlock())) {
-	//				if (remove) {
-	//					blocks.remove(rc);
-	//					affected++;
-	//				} else {
-	//					block.remove(rc.getBlock());
-	//				}
-	//			}
-	//		}
-	//		if (!remove) {
-	//			for (final Block b : block) {
-	//				if (specialBList.contains(b.getType())) {
-	//					specialBlocks.add(new RedBlockChild(b.getTypeId(), b.getData(), b.getLocation()));
-	//					affected++;
-	//					continue;
-	//				}
-	//				blocks.add(new RedBlockChild(b.getTypeId(), b.getData(), b.getLocation()));
-	//				affected++;
-	//			}
-	//		}
-	//		return affected;
-	//	}
-	//
-	//	/**
-	//	 * Gets a set of the RedBlockChilds controlled by the RedBlock.
-	//	 *
-	//	 * @return the set of RedBlockChilds
-	//	 */
-	//	public Set<RedBlockChild> getBlocks() {
-	//		final Set<RedBlockChild> b = new HashSet<RedBlockChild>();
-	//		b.addAll(blocks);
-	//		b.addAll(specialBlocks);
-	//		return b;
-	//	}
-	//
-	//	/**
-	//	 * Gets a RedBlockChild controlled by the RedBlock.
-	//	 *
-	//	 * @param b the block to get
-	//	 * @return the RedBlockChild
-	//	 */
-	//	public RedBlockChild getChild(final Block b) {
-	//		final Set<RedBlockChild> rbc = new HashSet<RedBlockChild>();
-	//		rbc.addAll(blocks);
-	//		final Set<RedBlockChild> rsbc = new HashSet<RedBlockChild>();
-	//		rsbc.addAll(specialBlocks);
-	//		for (final RedBlockChild child : rbc) {
-	//			if (child.getLocation().toString().equals(b.getLocation().toString())) {
-	//				return child;
-	//			}
-	//		}
-	//		for (final RedBlockChild child : rsbc) {
-	//			if (child.getLocation().toString().equals(b.getLocation().toString())) {
-	//				return child;
-	//			}
-	//		}
-	//		return null;
-	//	}
-	//
-	//	/**
-	//	 * Gets the RedBlock's location.
-	//	 *
-	//	 * @return the location
-	//	 */
-	//	public Location getLocation() {
-	//		final String str2loc[] = location.split("\\:");
-	//		final Location loc = new Location(Bukkit.getServer().getWorld(str2loc[0]), 0, 0, 0);
-	//		loc.setX(Double.parseDouble(str2loc[1]));
-	//		loc.setY(Double.parseDouble(str2loc[2]));
-	//		loc.setZ(Double.parseDouble(str2loc[3]));
-	//		return loc;
-	//	}
-	//
-	//	/**
-	//	 * Sets the Inverted property.
-	//	 *
-	//	 * @param bool the new Inverted property setting
-	//	 * @return the resulting property
-	//	 */
-	//	public boolean setInverted(final boolean bool) {
-	//		inverted = bool;
-	//		return inverted;
-	//	}
-	//
-	//	/**
-	//	 * Sets the Protected property.
-	//	 *
-	//	 * @param bool the new Protected property setting
-	//	 * @return the resulting property
-	//	 */
-	//	public boolean setProtected(final boolean bool) {
-	//		protect = bool;
-	//		return protect;
-	//	}
-	//
-	//	/**
-	//	 * Sets the Owner property.
-	//	 *
-	//	 * @param str the str
-	//	 * @return the resulting property
-	//	 */
-	//	public String setOwner(final String str) {
-	//		owner = str;
-	//		return owner;
-	//	}
-	//
-	//	/**
-	//	 * Sets if the RedBlock is under Redstone Timeout.
-	//	 *
-	//	 * @param bool the desired Redstone Timeout setting
-	//	 * @return the property
-	//	 */
-	//	public boolean setTimeout(final boolean bool) {
-	//		inTimeout = bool;
-	//		return bool;
-	//	}
-	//
-	//	/**
-	//	 * Checks if the RedBlock is already enabled.
-	//	 *
-	//	 * @return if the RedBlock is enabled
-	//	 */
-	//	public boolean isEnabled() {
-	//		return blocksActive;
-	//	}
-	//
-	//	/**
-	//	 * Gets the Block of the RedBlock.
-	//	 *
-	//	 * @return the Block of the RedBlock
-	//	 */
-	//	public Block getBlock() {
-	//		return getLocation().getBlock();
-	//	}
-	//
-	//	/**
-	//	 * Gets the number of blocks controlled by the RedBlock.
-	//	 *
-	//	 * @return the number of blocks
-	//	 */
-	//	public int getBlockCount() {
-	//		return blocks.size() + specialBlocks.size();
-	//	}
-	//
-	//	/**
-	//	 * Gets the typeId of the RedBlock.
-	//	 *
-	//	 * @return the typeId of the RedBlock
-	//	 */
-	//	public int getTypeId() {
-	//		return typeId;
-	//	}
-	//
-	//	/**
-	//	 * Gets the data of the RedBlock.
-	//	 *
-	//	 * @return the data of the RedBlock
-	//	 */
-	//	public byte getData() {
-	//		return data;
-	//	}
-	//
-	//	/**
-	//	 * Gets the Owner property.
-	//	 *
-	//	 * @return the Owner property
-	//	 */
-	//	public String getOwner() {
-	//		return owner;
-	//	}
-	//
-	//	/**
-	//	 * Gets the Protected property.
-	//	 *
-	//	 * @return the protected property
-	//	 */
-	//	public boolean isProtected() {
-	//		return protect;
-	//	}
-	//
-	//	/**
-	//	 * Gets the Inverted property.
-	//	 *
-	//	 * @return the Inverted property
-	//	 */
-	//	public boolean isInverted() {
-	//		return inverted;
-	//	}
-	//
-	//	/**
-	//	 * Gets the inTimeOut property.
-	//	 *
-	//	 * @return the inTimeOut property
-	//	 */
-	//	public boolean isInTimeout() {
-	//		return inTimeout;
-	//	}
-	//
-	//	/**
-	//	 * Gets the changed property.
-	//	 *
-	//	 * @return the changed property
-	//	 */
-	//	public boolean hasChanged() {
-	//		return changed;
-	//	}
-	//
-	//	/**
-	//	 * Sets the changed property.
-	//	 *
-	//	 * @param bool the new changed property
-	//	 */
-	//	public void setChanged(final boolean bool) {
-	//		changed = bool;
-	//	}
-	//
-	//	/**
-	//	 * Convert location.
-	//	 *
-	//	 * @param loc the loc
-	//	 * @return the string
-	//	 */
-	//	private String convertLocation(final Location loc) {
-	//		return loc.getWorld().getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ();
-	//	}
-	//
-	//	/**
-	//	 * Fix nulls.
-	//	 */
-	//	private void fixNulls() {
-	//		if (specialBlocks == null) {
-	//			specialBlocks = new HashSet<RedBlockChild>();
-	//		}
-	//		if (blocks == null) {
-	//			blocks = new HashSet<RedBlockChild>();
-	//		}
-	//	}
 }
