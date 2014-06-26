08ff61d0bbc7650f8dbd3054e393b75b744cd1a4
Techzune <techzune@operontech.com>
2014-06-26 14:19:40 -0500

    Chat message formatting for performance

--- src/main/java/com/operontech/redblocks/listener/CommandListener.java
+++ src/main/java/com/operontech/redblocks/listener/CommandListener.java
@@ -62,17 +62,29 @@ public class CommandListener {
 						} else if (Util.multiString(args[0], "stop", "quit", "s")) {
 							plugin.removeEditor(p);
 						} else if (Util.multiString(args[0], "delay", "d")) {
-							if (args.length >= 3) {
-								if (Util.multiString(args[1], "place", "p", "break", "b")) {
+							if (args.length >= 2) {
+								if (Util.multiString(args[1], "place", "p", "enable", "e", "break", "b", "disable", "d")) {
 									final PlayerSession session = plugin.getPlayerSession(p);
+									if (args.length < 3) {
+										if (Util.multiString(args[1], "place", "p", "enable", "e")) {
+											session.setEnableDelay("0");
+											session.setEnableDelayBlock("0", "-1");
+											console.notify(s, ChatColor.YELLOW + "No future placed blocks will be delay enabling.");
+										} else {
+											session.setDisableDelay("0");
+											session.setDisableDelayBlock("0", "-1");
+											console.notify(s, ChatColor.YELLOW + "No future placed blocks will be delay disabling.");
+										}
+										return true;
+									}
 									String tempText;
-									if (Util.isInteger(args[2])) {
-										if (Util.multiString(args[1], "place", "p")) {
+									if (Util.isInteger(args[2]) && (args.length == 3)) {
+										if (Util.multiString(args[1], "place", "p", "enable", "e")) {
 											session.setEnableDelay(args[2]);
-											console.notify(s, "All future child blocks you place will delay on enabling for " + args[2] + " milliseconds.");
+											console.notify(s, ChatColor.YELLOW + "Future placed blocks will delay enabling for", ChatColor.GOLD + args[2] + ChatColor.YELLOW + " milliseconds.");
 										} else {
 											session.setDisableDelay(args[2]);
-											console.notify(s, "All future child blocks you place will delay on disabling for " + args[2] + " milliseconds.");
+											console.notify(s, ChatColor.YELLOW + "Future placed blocks will delay disabling for", ChatColor.GOLD + args[2] + ChatColor.YELLOW + " milliseconds.");
 										}
 									}
 									if (args.length > 3) {
@@ -80,21 +92,15 @@ public class CommandListener {
 											tempText = args[i].toLowerCase();
 											if (tempText.startsWith("block:") || tempText.startsWith("b:")) {
 												final String[] splitText = tempText.split(":");
-												if ((splitText.length == 2) && Util.isInteger(splitText[1])) {
-													if (Util.multiString(args[1], "place", "p")) {
-														session.setEnableDelayBlock(splitText[1], "-1");
-														console.notify(s, "Future placed blocks will delay enabling if they are block id " + ChatColor.GOLD + splitText[1]);
+												if (Util.isInteger(splitText[1])) {
+													if (Util.multiString(args[1], "place", "p", "enable", "e")) {
+														session.setEnableDelay(args[2]);
+														session.setEnableDelayBlock(splitText[1], ((splitText.length > 2) && Util.isInteger(splitText[2]) ? splitText[2] : "-1"));
+														console.notify(s, ChatColor.YELLOW + "Future placed blocks will delay enabling for", ChatColor.GOLD + args[2] + ChatColor.YELLOW + " milliseconds if the block has the id: " + ChatColor.GOLD + splitText[1] + ((splitText.length > 2) && Util.isInteger(splitText[2]) ? ":" + splitText[2] : ""));
 													} else {
-														session.setDisableDelayBlock(splitText[1], "-1");
-														console.notify(s, "Future placed blocks will delay disabling if they are block id " + ChatColor.GOLD + splitText[1]);
-													}
-												} else if ((splitText.length == 3) && Util.isInteger(splitText[1]) && Util.isInteger(splitText[2])) {
-													if (Util.multiString(args[1], "place", "p")) {
-														session.setEnableDelayBlock(splitText[1], splitText[2]);
-														console.notify(s, "Future placed blocks will delay enabling if they are block id " + ChatColor.GOLD + splitText[1] + ":" + splitText[2]);
-													} else {
-														session.setDisableDelayBlock(splitText[1], splitText[2]);
-														console.notify(s, "Future placed blocks will delay disabling if they are block id " + ChatColor.GOLD + splitText[1] + ":" + splitText[2]);
+														session.setDisableDelay(args[2]);
+														session.setDisableDelayBlock(splitText[1], ((splitText.length > 2) && Util.isInteger(splitText[2]) ? splitText[2] : "-1"));
+														console.notify(s, ChatColor.YELLOW + "Future placed blocks will delay disabling for", ChatColor.GOLD + args[2] + ChatColor.YELLOW + " milliseconds if the block has the id: " + ChatColor.GOLD + splitText[1] + ((splitText.length > 2) && Util.isInteger(splitText[2]) ? ":" + splitText[2] : ""));
 													}
 												} else {
 													sendCMenu(s);
@@ -179,30 +185,30 @@ public class CommandListener {
 	private void sendCMenu(final CommandSender s) {
 		console.msg(s, ChatColor.GOLD + "   >>>>> RedBlocks Menu <<<<<   ");
 		if (plugin.hasPermission(s, "reload")) {
-			console.msg(s, ChatColor.GREEN + "Reload RedBlocks:", ChatColor.LIGHT_PURPLE + "     /rb reload");
+			console.msg(s, ChatColor.GREEN + "Reload RedBlocks:", "     /rb reload");
 		}
-		console.msg(s, ChatColor.GREEN + "Stop Editing RedBlock:", ChatColor.LIGHT_PURPLE + "     /rb stop");
-		console.msg(s, ChatColor.GREEN + "Set Pause For Single Child Block:", ChatColor.LIGHT_PURPLE + "     /rb p <place/break> <MILLISECONDS>");
-		console.msg(s, ChatColor.GREEN + "Set Pause For Multiple Child Blocks:", ChatColor.LIGHT_PURPLE + "     /rb pm <place/break> <MILLISECONDS>");
-		console.msg(s, ChatColor.GREEN + "Stop Editing RedBlock:" + ChatColor.LIGHT_PURPLE, "     /rb stop");
-		console.msg(s, ChatColor.GREEN + "Edit Options:" + ChatColor.LIGHT_PURPLE, "     /rb options <OPTION> <VALUE>");
-		console.msg(s, ChatColor.GREEN + "Set RedBlockChild Delays:" + ChatColor.LIGHT_PURPLE, "     /rb delay [place/break] [MILLISECONDS] <block:ID:DATA>");
+		console.msg(s, ChatColor.GREEN + "Stop Editing RedBlock:", "     /rb stop");
+		console.msg(s, ChatColor.GREEN + "Set Pause For Single Child Block:", "     /rb p <place/break> <MILLISECONDS>");
+		console.msg(s, ChatColor.GREEN + "Set Pause For Multiple Child Blocks:", "     /rb pm <place/break> <MILLISECONDS>");
+		console.msg(s, ChatColor.GREEN + "Stop Editing RedBlock:", "     /rb stop");
+		console.msg(s, ChatColor.GREEN + "Edit Options:", "     /rb options <OPTION> <VALUE>");
+		console.msg(s, ChatColor.GREEN + "Set RedBlockChild Delays:", "     /rb delay [place/break] [MILLISECONDS] <block:ID:DATA>");
 		if (plugin.hasPermission(s, "worldedit") && (plugin.getWE() != null)) {
-			console.msg(s, ChatColor.GREEN + "World-Edit: AddCommand Blocks:" + ChatColor.LIGHT_PURPLE, "     /rb add [TYPE:DMG]");
-			console.msg(s, ChatColor.GREEN + "World-Edit: RemoveCommand Blocks:" + ChatColor.LIGHT_PURPLE, "     /rb remove [TYPE:DMG]");
+			console.msg(s, ChatColor.GREEN + "World-Edit: AddCommand Blocks:", "     /rb add [TYPE:DMG]");
+			console.msg(s, ChatColor.GREEN + "World-Edit: RemoveCommand Blocks:", "     /rb remove [TYPE:DMG]");
 		}
 	}
 
 	private void sendCOptions(final CommandSender s) {
 		console.msg(s, ChatColor.GOLD + "   >>>>> Options for RedBlocks <<<<<   ");
 		if (plugin.hasPermission(s, "optionsInverted")) {
-			console.msg(s, ChatColor.GREEN + "Inverted Redstone:" + ChatColor.LIGHT_PURPLE, "     /rb options inverted [default/true/false]");
+			console.msg(s, ChatColor.GREEN + "Inverted Redstone:", "     /rb options inverted [default/true/false]");
 		}
 		if (plugin.hasPermission(s, "optionsProtect")) {
-			console.msg(s, ChatColor.GREEN + "Protect Blocks:" + ChatColor.LIGHT_PURPLE, "     /rb options protect [true/false]");
+			console.msg(s, ChatColor.GREEN + "Protect Blocks:", "     /rb options protect [true/false]");
 		}
 		if (plugin.hasPermission(s, "optionsOwner")) {
-			console.msg(s, ChatColor.GREEN + "RedBlock Owner:" + ChatColor.LIGHT_PURPLE, "     /rb options owner [NAME]");
+			console.msg(s, ChatColor.GREEN + "RedBlock Owner:", "     /rb options owner [NAME]");
 			console.msg(s, ChatColor.RED + "     Warning: This cannot be undone. Both players must be online.");
 		}
 	}
