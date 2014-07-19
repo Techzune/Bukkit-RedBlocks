32555b06679c067536871e43c38a71633b1036ef
Techzune <techzune@operontech.com>
2014-07-19 12:46:07 -0500

    OCD Formatting

--- src/main/java/com/operontech/redblocks/playerdependent/Permission.java
+++ src/main/java/com/operontech/redblocks/playerdependent/Permission.java
@@ -5,30 +5,30 @@ import org.bukkit.command.CommandSender;
 public enum Permission {
 	USE(
 			"use"),
-			ANIMATIONS(
-					"animations"),
-					RELOAD(
-							"reload"),
-							WORLDEDIT(
-									"worldedit"),
-									DELAY(
-											"delay"),
-											CREATEANDDESTROY(
-													"createanddestroy"),
-													BYPASS_PROTECT(
-															"bypass.protect"),
-															BYPASS_WEMAX(
-																	"bypass.WEMax"),
-																	BYPASS_MAXBLOCKSPER(
-																			"bypass.maxBlocksPer"),
-																			BYPASS_MAXREDBLOCKSPER(
-																					"bypass.maxRedBlocksPer"),
-																					OPTIONS_PROTECT(
-																							"options.protect"),
-																							OPTIONS_INVERTED(
-																									"options.inverted"),
-																									OPTIONS_OWNER(
-																											"options.owner");
+	ANIMATIONS(
+			"animations"),
+	RELOAD(
+			"reload"),
+	WORLDEDIT(
+			"worldedit"),
+	DELAY(
+			"delay"),
+	CREATEANDDESTROY(
+			"createanddestroy"),
+	BYPASS_PROTECT(
+			"bypass.protect"),
+	BYPASS_WEMAX(
+			"bypass.WEMax"),
+	BYPASS_MAXBLOCKSPER(
+			"bypass.maxBlocksPer"),
+	BYPASS_MAXREDBLOCKSPER(
+			"bypass.maxRedBlocksPer"),
+	OPTIONS_PROTECT(
+			"options.protect"),
+	OPTIONS_INVERTED(
+			"options.inverted"),
+	OPTIONS_OWNER(
+			"options.owner");
 	private String pString;
 
 	private Permission(final String pString) {
