d7cf570b8b3059d4ca97ca4278f87355db670aba
Techzune <techzune@operontech.com>
2014-07-19 12:45:43 -0500

    OCD Formatting

--- src/main/java/com/operontech/redblocks/ConfigValue.java
+++ src/main/java/com/operontech/redblocks/ConfigValue.java
@@ -3,34 +3,34 @@ package com.operontech.redblocks;
 public enum ConfigValue {
 	configVersion(
 			"configVersion"),
-			updateCheck(
-					"updateCheck"),
-					rules_maxBlocksPer(
-							"rules.maxBlocksPer"),
-							rules_maxRedBlocksPer(
-									"rules.maxRedBlocksPer"),
-									redblocks_blockID(
-											"redblocks.blockID"),
-											redblocks_drops(
-													"redblocks.drops"),
-													redblocks_soundFX(
-															"redblocks.soundFX"),
-															redblocks_destroyItem(
-																	"redblocks.destroyItem"),
-																	redblocks_redstoneTimeout(
-																			"redblocks.redstoneTimeout"),
-																			worldedit_maxAtOnce(
-																					"worldedit.maxAtOnce"),
-																					worldedit_preventBedrock(
-																							"worldedit.preventBedrock"),
-																							gc_onWorldEdit(
-																									"gc.onWorldEdit"),
-																									gc_onDisableRedBlock(
-																											"gc.onDisableRedBlock"),
-																											gc_onEnableRedBlock(
-																													"gc.onEnableRedBlock"),
-																													gc_onDestroyRedBlock(
-																															"gc.onDestroyRedBlock");
+	updateCheck(
+			"updateCheck"),
+	rules_maxBlocksPer(
+			"rules.maxBlocksPer"),
+	rules_maxRedBlocksPer(
+			"rules.maxRedBlocksPer"),
+	redblocks_blockID(
+			"redblocks.blockID"),
+	redblocks_drops(
+			"redblocks.drops"),
+	redblocks_soundFX(
+			"redblocks.soundFX"),
+	redblocks_destroyItem(
+			"redblocks.destroyItem"),
+	redblocks_redstoneTimeout(
+			"redblocks.redstoneTimeout"),
+	worldedit_maxAtOnce(
+			"worldedit.maxAtOnce"),
+	worldedit_preventBedrock(
+			"worldedit.preventBedrock"),
+	gc_onWorldEdit(
+			"gc.onWorldEdit"),
+	gc_onDisableRedBlock(
+			"gc.onDisableRedBlock"),
+	gc_onEnableRedBlock(
+			"gc.onEnableRedBlock"),
+	gc_onDestroyRedBlock(
+			"gc.onDestroyRedBlock");
 
 	private String configString;
 
